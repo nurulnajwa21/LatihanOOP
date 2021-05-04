6d70405506c5d2cca6295a6c43a1589573febcdf
Nurul Najwa Binti Boti <18DDT19F1014@student.ptss.edu.my>
2021-05-05 04:12:08 +0800

    Add project files.

---
+++ LatihanOOP/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub BtnPapar_Click(sender As Object, e As EventArgs) Handles BtnPapar.Click
+        Dim strNoMatrik As String = txtNoMatrik.Text
+
+        If strNoMatrik.Length < 12 Then
+            MessageBox.Show("Nombor matrik terlalu pendek")
+        Else
+            Dim PelajarBaru As New Pelajar(strNoMatrik)
+            txtPoliteknik.Text = PelajarBaru.Politeknik
+            txtProgram.Text = PelajarBaru.Program
+            txtAmbilan.Text = PelajarBaru.Ambilan
+        End If
+    End Sub
+
+    Private Sub txtNoMatrik_TextChanged(sender As Object, e As EventArgs) Handles txtNoMatrik.TextChanged
+
+    End Sub
+End Class
---
+++ LatihanOOP/Pelajar.vb
@@ -0,0 +1,37 @@
+﻿Public Class Pelajar
+    Private strNoMatrik As String
+    Sub New(ByVal strNoMatrik As String)
+        Me.strNoMatrik = strNoMatrik
+    End Sub
+
+    Public Function Politeknik() As String
+        Dim strKodPoli As String = strNoMatrik.Substring(0, 2)
+
+        If strKodPoli = 18 Then
+            Return "PTSS"
+        Else
+            Return "NOT PTSS "
+        End If
+    End Function
+
+    Public Function Ambilan() As String
+        Dim strKodAmbilan As String = strNoMatrik.Substring(5, 3)
+        Dim strKodTahun As String = strNoMatrik.Substring(8, 1)
+        If strKodAmbilan = "19F" And strKodTahun = "1" Then
+            Return "JUN 2019"
+        ElseIf strKodAmbilan = "19F" And strKodTahun = "2" Then
+            Return "DEC 2019"
+        Else
+            Return "Not Sesi 2019"
+        End If
+    End Function
+
+    Public Function Program() As String
+        Dim strKodProgram As String = strNoMatrik.Substring(2, 3)
+        If strKodProgram = "DDT" Then
+            Return ("Diploma Teknologi Maklumat (Teknologi Digital)")
+        Else
+            Return ("Bukan Diploma Teknologi Maklumat (Teknologi Digital)")
+        End If
+    End Function
+End Class
